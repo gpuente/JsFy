2b8fcaa9178ab1352a60ab41e31eb26604f1809c
Guillermo Puente <guillermo.ps09@gmail.com>
2017-04-08 22:13:09 -0300

    Added auth api middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,1 +1,20 @@
 'use strict'
+
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var secret_password = global.config.jwt.secret_password; //define this password in config.json
+
+exports.checkApiAuth = function(req, res, next){
+	if(!req.headers.authorization) return res.status(403).send({message: global.st.error_login_header});
+	var token = req.headers.authorization.replace(/['"]+/g, '');
+	try{
+		var payload = jwt.decode(token, secret_password);
+		if(payload.exp < moment().unix()) return res.status(401).send({message: global.st.error_login_token_expired});
+	}catch(e){
+		//console.log(e);
+		return res.status(404).send({message: global.st.error_login_invalid_token});
+	}
+
+	req.user = payload;
+	next();
+};
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,10 +2,11 @@
 
 var express = require('express');
 var UserController = require('../controllers/UserController');
+var mdAuth = require('../middlewares/auth');
 
 var api = express.Router();
 
-api.get('/prueba-controlador', UserController.pruebas);
+api.get('/prueba-controlador', mdAuth.checkApiAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 
