23db41a9df7fb8ac2981dd4da3336afa8461477f
Guillermo Puente <gpuente@borealis.cl>
2017-04-12 16:49:07 -0300

    Added dynamic load for jwt middleware. The load depends of the configuration env file

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,8 +4,11 @@ var jwt = require('jwt-simple');
 var moment = require('moment');
 var config = require('config');
 var secret_password = config.get('jwt.secret_password');
+let enabled = config.get('middleware.jwt.load');
 
 exports.checkApiAuth = function(req, res, next){
+	if(!enabled) return next();
+
 	if(!req.headers.authorization) return res.status(403).send({message: global.st.error_login_header});
 	var token = req.headers.authorization.replace(/['"]+/g, '');
 	try{
