60734f71e53e6208897757943d975a02c830b604
Guillermo Puente <guillermo.ps09@gmail.com>
2017-04-13 00:44:46 -0300

    Added test for /PUT user

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ var Schema = mongoose.Schema;
 var UserSchema = Schema({
 	name: {type: String, required: true},
 	surname: {type: String, required: true},
-	email: {type: String, required: true},
+	email: {type: String, required: true, unique: true},
 	password: {type: String, required: true},
 	role: String,
 	image: String
--- test/users.js
+++ test/users.js
@@ -2,6 +2,7 @@ process.env.NODE_ENV = 'test';
 
 let mongoose = require('mongoose');
 let User = require('../models/User');
+let bcrypt = require('bcrypt-nodejs');
 
 let chai = require('chai');
 let chaiHttp = require('chai-http');
@@ -12,7 +13,7 @@ let st = require('../lang/strings_en.json');
 
 chai.use(chaiHttp);
 
-describe('Users', () => {
+describe('Users:', () => {
 
 	beforeEach((done) => {
 		User.remove({}, (err) => {
@@ -21,7 +22,7 @@ describe('Users', () => {
 	});
 
 	describe('/POST register', () => {
-		it('it no should register a new user without required data', (done) => {
+		it('it should not register a new user without required data', (done) => {
 			let user = {
 				name: "Alfredo Ramirez",
 				surname: "aramirez"
@@ -33,6 +34,27 @@ describe('Users', () => {
 					res.should.have.status(206);
 					res.body.should.have.a('object');
 					res.body.should.have.property('message').eql(st.user_incomplete);
+					done();
+				});
+		});
+
+		it('it should not register a duplicated user', (done) => {
+			let user = new User({
+				name: 'Fran Chain',
+				surname: 'fchain',
+				email: 'fchain@gmail.com',
+				password: '123456',
+				role: 'ROLE_ADMIN',
+				image: 'image.png'
+			});
+			user.save();
+			chai.request(server)
+				.post('/api/register')
+				.send(user)
+				.end((err, res) => {
+					res.should.have.status(500);
+					res.body.should.have.a('object');
+					res.body.should.have.property('message').eql(st.user_no_registered);
 					done();
 				});
 		});
@@ -58,6 +80,7 @@ describe('Users', () => {
 					res.body.user.should.have.property('role').eql('ROLE_ADMIN');
 					res.body.user.should.have.property('image').eql('null');
 					res.body.user.should.have.property('password');
+					(bcrypt.compareSync(user.password, res.body.user.password)).should.to.be.true;
 					done();
 				});
 		});
@@ -66,7 +89,7 @@ describe('Users', () => {
 
 
 	describe('/POST login', () => {
-		it('it should not loggin a user with missing data',(done) => {
+		it('it should not login a user with missing data',(done) => {
 			let user = {
 				email: "usernotexist@gmail.com"
 			};
@@ -82,7 +105,7 @@ describe('Users', () => {
 		});
 
 
-		it('it should not loggin a user with incorrect credentialas', (done) => {
+		it('it should not login a user with incorrect credentialas', (done) => {
 			let user = new User({
 				name: 'Fran Chain',
 				surname: 'fchain',
@@ -125,12 +148,107 @@ describe('Users', () => {
 					res.body.user.should.have.property('_id');
 					res.body.user.should.have.property('email').eql(user.email);
 					res.body.user.should.have.property('name').eql(user.name);
+					res.body.user.password.should.be.eql(user.password);
 					done();
 				});
 			});
 		});
 
 
+		it('it should login a user with right credentials and return token requested', (done) => {
+			let user = new User({
+				name: 'Fran Chain',
+				surname: 'fchain',
+				email: 'fchain@gmail.com',
+				password: '123456',
+				role: 'ROLE_ADMIN',
+				image: 'image.png'
+			});
+			user.save((err, user) => {
+				chai.request(server)
+				.post('/api/login')
+				.send({email: 'fchain@gmail.com', password: '123456', gethash: 'true'})
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.a('object');
+					res.body.should.have.property('token');
+					done();
+				});
+			});
+		});
+
+	});
+
+
+	describe('/PUT user', () => {
+		it('it should not update an user with invalid user id', (done) => {
+			let user = new User({
+				name: 'Fran Chain',
+				surname: 'fchain',
+				email: 'fchain@gmail.com',
+				password: '123456',
+				role: 'ROLE_ADMIN',
+				image: 'image.png'
+			});
+			user.save();
+			chai.request(server)
+				.put('/api/user/' + 'notvalidid')
+				.send({name: 'Fran Kum', surname: 'fkun'})
+				.end((err, res) => {
+					res.should.have.status(500);
+					res.body.should.have.a('object');
+					res.body.should.have.property('message').eql(st.error_update_user);;
+					done();
+				});
+
+		});
+
+
+		it('it should not update an user with id user not registered', (done) => {
+			let user = new User({
+				name: 'Fran Chain',
+				surname: 'fchain',
+				email: 'fchain@gmail.com',
+				password: '123456',
+				role: 'ROLE_ADMIN',
+				image: 'image.png'
+			});
+			user.save();
+			chai.request(server)
+				.put('/api/user/' + '58eeeda1c345071010095a09')
+				.send({name: 'Fran Kum', surname: 'fkun'})
+				.end((err, res) => {
+					res.should.have.status(404);
+					res.body.should.have.a('object');
+					res.body.should.have.property('message').eql(st.user_not_updated);
+					done();
+				});
+
+		});
+
+
+		it('it should update an user', (done) => {
+			let user = new User({
+				name: 'Fran Chain',
+				surname: 'fchain',
+				email: 'fchain@gmail.com',
+				password: '123456',
+				role: 'ROLE_ADMIN',
+				image: 'image.png'
+			});
+			user.save();
+			chai.request(server)
+				.put('/api/user/' + user.id)
+				.send({name: 'Fran Kum', surname: 'fkun'})
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.a('object');
+					res.body.should.have.property('user');
+					res.body.user.should.have.property('_id').eql(user.id);
+					done();
+				});
+
+		});
 
 	});
 
